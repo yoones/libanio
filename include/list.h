984f29ff84071fae90077e78ef6ab03252035f95
Younes SERRAJ <younes.serraj@gmail.com>
2015-07-28 21:42:29 +0200

    Added license (GPL)

--- include/list.h
+++ include/list.h
@@ -1,17 +1,22 @@
 /*
-**
-**
+  This file is part of libanio (https://github.com/yoones/libanio).
+
+  libanio is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  libanio is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with libanio.  If not, see <http://www.gnu.org/licenses/>
 */
 
 #ifndef LIST_H_
 # define LIST_H_
-
-/* typedef enum		e_ldatatype */
-/*   { */
-/*     LDT_INT, */
-/*     LDT_STRING, */
-/*     LDT_ */
-/*   }			t_ldatatype; */
 
 typedef struct		s_lnode
 {
--- src/init/set_callback_on_eof.c
+++ src/init/set_callback_on_eof.c
@@ -1,3 +1,20 @@
+/*
+  This file is part of libanio (https://github.com/yoones/libanio).
+
+  libanio is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  libanio is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with libanio.  If not, see <http://www.gnu.org/licenses/>
+*/
+
 #include "libanio.h"
 
 static void	_do_nothing(t_anio *server, int fd, char *buf, size_t size)
